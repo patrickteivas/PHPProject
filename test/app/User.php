dacc404c60f4772184ff0fa4821232d7689b919a
patrickteivas <teivas.2011@mail.ru>
2019-01-29 12:35:56 +0200

    Commit

---
+++ test/app/User.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: opilane
+ * Date: 29.01.2019
+ * Time: 11:42
+ */
+
+namespace App;
+
+
+class User
+{
+
+}
---
+++ test/bin/run.php
@@ -0,0 +1,5 @@
+<?php
+require __DIR__ . '/../vendor/autoload.php';
+
+$user = new \App\User();
+var_dump($user);
